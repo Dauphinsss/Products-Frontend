ef38fe41156cca925d28502f50f0b430a5b7830b
Marko <marcosvelasquezvela123@gmail.com>
2025-01-07 12:38:06 -0400

    creo que ya

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,33 +1,55 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { getProduct } from "../new/products.api";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Product } from "@/app/dto/dtos";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
+import { useParams } from "next/navigation";
 
-interface ProductPageProps {
-  params: { id: string };
+interface Params {
+  id?: string;
 }
 
-export default async function ProductDetail({ params }: ProductPageProps) {
-  const product: Product = await getProduct(params.id);
+export default function ProductDetail() {
+  const params = useParams() as Params;
+  const { id } = params;
+  console.log(id);
+  const [product, setProduct] = useState<Product>();
+
+  useEffect(() => {
+    if (id) {
+      const fetchProduct = async () => {
+        const productData = await getProduct(id);
+        console.log(productData);
+        setProduct(productData);
+      };
+      fetchProduct();
+    }
+  }, [id]);
 
   return (
     <div className="flex justify-center items-center h-screen">
       <Card>
         <CardHeader>
           <CardTitle className="flex justify-between">
-            Product Detail: {product.id}
+            Product Detail: {product?.id || "Loading..."}
             <Link href="/" className={buttonVariants()}>
               Go back
             </Link>
           </CardTitle>
         </CardHeader>
         <CardContent>
-          <h1>{product.name}</h1>
-          <p>{product.description}</p>
-          <p>${product.price}</p>
-          <img src={product.image} className="object-contain w-96 m-5" />
+          {product ? (
+            <>
+              <h1>{product.name}</h1>
+              <p>{product.description}</p>
+              <p>${product.price}</p>
+              <img src={product.image} className="object-contain w-96 m-5" />
+            </>
+          ) : (
+            <p>Loading product...</p>
+          )}
         </CardContent>
       </Card>
     </div>
